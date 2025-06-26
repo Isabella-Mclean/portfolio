52110e120e2363116ff0c4771f60cb7211f64bd2
IsabellaMclean <80723693+Isabella-Mclean@users.noreply.github.com>
2025-06-26 20:32:49 +0100

    Add user event library for testing, increase coverage of the carousel

--- portfolio-website/src/__tests__/app/page.test.tsx
+++ portfolio-website/src/__tests__/app/page.test.tsx
@@ -1,8 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Page from "../../app/page";
 import React from "react";
 import { test } from 'vitest';
+import userEvent from '@testing-library/user-event'
+
 
 test('renders home page', () => {
     render(<Page/>);
 });
+
+test('carousel arrow buttons work correctly', async () => {
+    const user = userEvent.setup()
+    render(<Page/>);
+    const leftArrowButton = screen.getAllByRole("button")[5];
+    const rightArrowButton = screen.getAllByRole("button")[6];
+    await user.click(leftArrowButton);
+    await user.click(rightArrowButton);
+});
+
+test('carousel image button works correctly', async () => {
+    const user = userEvent.setup()
+    render(<Page/>);
+    const imageButton = screen.getAllByRole("button")[1];
+    await user.click(imageButton);
+});
+
