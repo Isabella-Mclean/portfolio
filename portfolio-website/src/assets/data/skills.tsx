b0153ae0a200bfd4f03e6456a9279046f8d3c4b2
IsabellaMclean <80723693+Isabella-Mclean@users.noreply.github.com>
2025-12-04 15:20:59 +0000

    Update skills

--- portfolio-website/src/assets/data/skills.tsx
+++ portfolio-website/src/assets/data/skills.tsx
@@ -1,14 +1,18 @@
 export const skills = [
     {
-        description: "Python, Java, SQL, Scala, C++, ROS, React.js, Typescript, C#",
+        description: "Python, Java, SQL, Scala, ROS, Typescript, C#, Kotlin",
         title: "Programming languages",
     },
     {
-        description: "TailwindCSS, Linux, Windows, MAC, Github, Jest",
+        description: "React, Next.js, Django, Ktor, .NET, Vitetest, Jest",
+        title: "Frameworks & Libraries",
+    },
+    {
+        description: "TailwindCSS, Linux, Windows, MAC, Github, Jira, Firebase",
         title: "Tools",
     },
     {
-        description: "Project management, B1 German, Teamwork, Leadership, AGILE Workflows",
+        description: "Project management, B1 German, Teamwork, Leadership, AGILE Workflows, Interviewing",
         title: "Non-technical",
     },
 ]
